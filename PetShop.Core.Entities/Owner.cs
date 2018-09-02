d3a2ecd648ce6a60e09e8ae68ea746764c17980e
pallemjensen <31887698+pallemjensen@users.noreply.github.com>
2018-09-02 17:28:34 +0200

    Initial commit

---
+++ PetShop.Core.Entities/Owner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Entities
+{
+    public class Owner
+    {
+        public int OwnerId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Address { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    
+}
